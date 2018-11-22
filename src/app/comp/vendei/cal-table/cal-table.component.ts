6a07856b04bda8ea3f739dd68aac2c6bf7d99055
lartl <luis.arce22@gmail.com>
2018-11-21 20:08:15 -0400

    Use bill images from assets

--- src/app/comp/vendei/cal-table/cal-table.component.ts
+++ src/app/comp/vendei/cal-table/cal-table.component.ts
@@ -29,57 +29,50 @@ export class CalTableComponent implements OnInit {
   selectedCustomer: any;
 
   bills = [
-    { name: "0.1", value: 0.1, img: "" },
-    { name: "0.2", value: 0.2, img: "" },
     {
       name: "0.5",
       value: 0.5,
-      img: "https://en.numista.com/catalogue/photos/bolivie/g397.jpg"
+      img: "0-50.jpg"
     },
     {
       name: "1",
       value: 1,
-      img:
-        "https://i.ucoin.net/coin/15/048/15048242-1/bolivia-1-boliviano-2012.jpg"
+      img: "/assets/vendei/money/1-Boliviano.jpg"
     },
     {
       name: "2",
       value: 2,
-      img: "https://i.colnect.net/f/4353/166/2-Bolivianos-Large-Type.jpg"
+      img: "/assets/vendei/money/2-Bolivianos.jpg"
     },
     {
       name: "5",
       value: 5,
-      img: "https://i.colnect.net/f/2815/611/5-Bolivianos.jpg"
+      img: "/assets/vendei/money/5-Bolivianos.jpg"
     },
     {
       name: "10",
       value: 10,
-      img:
-        "http://www.lostiempos.com/sites/default/files/styles/noticia_detalle/public/media_imagen/2018/4/4/sin_titulo-1_0.jpg?itok=xuBQscCf"
+      img: "/assets/vendei/money/10-Bolivianos.jpg"
     },
     {
       name: "20",
       value: 20,
-      img: "https://media2.allnumis.com/1189_572233078c3986fL.jpg"
+      img: "/assets/vendei/money/20-Bolivianos.jpg"
     },
     {
       name: "50",
       value: 50,
-      img:
-        "https://img.ma-shops.com/aurich/pic/14169_bolivien_50_bolivianos_28.11.1986_bn_.jpg"
+      img: "/assets/vendei/money/50-Bolivianos.jpg"
     },
     {
       name: "100",
       value: 100,
-      img:
-        "https://www.paginasiete.bo/u/fotografias/m/2017/9/27/f800x450-180473_231919_0.jpg"
+      img: "/assets/vendei/money/100-Bolivianos.jpeg"
     },
     {
       name: "200",
       value: 200,
-      img:
-        "https://www.eldeber.com.bo/__export/1481083317747/sites/eldeber/img/2015/07/02/5595aa693d5f4.jpeg_1775534641.jpeg"
+      img: "/assets/vendei/money/200-Bolivianos.jpeg"
     }
   ];
 
