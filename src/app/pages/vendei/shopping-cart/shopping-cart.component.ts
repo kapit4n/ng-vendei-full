8e2e4bb8afaa2d49dcbc1636f1d490750ed4c958
Luis Arce <luis.arce22@gmail.com>
2019-06-05 23:27:47 -0400

    Upgrade to angular 8

--- src/app/pages/vendei/shopping-cart/shopping-cart.component.ts
+++ src/app/pages/vendei/shopping-cart/shopping-cart.component.ts
@@ -72,7 +72,7 @@ export class ShoppingCartComponent implements OnInit {
     });
   }
 
-  @ViewChild("toPrint") myDiv: ElementRef;
+  @ViewChild("toPrint", {static: false}) myDiv: ElementRef;
 
   ngOnInit() {}
 
